ec5e6041766d7892def0ad805e335947db4c107c
HADES-96 <108996607+HADES-96@users.noreply.github.com>
2022-11-07 11:52:37 +0100

    little translate change in for german
    litte changes (EUR and better sentences)

--- [esx_addons]/esx_clotheshop/locales/de.lua
+++ [esx_addons]/esx_clotheshop/locales/de.lua
@@ -1,12 +1,12 @@
 Locales['de'] = {
   ['valid_this_purchase'] = 'Einkauf bestätigen?',
-  ['yes'] = 'ja',
-  ['no'] = 'nein',
+  ['yes'] = 'Ja',
+  ['no'] = 'Nein',
   ['not_enough_money'] = 'Du hast nicht genug Geld',
   ['press_menu'] = 'Drücke [E] um das Menü zu öffnen',
   ['clothes'] = 'Kleidung',
-  ['you_paid'] = 'Du bezahlst $%s',
-  ['save_in_dressing'] = 'Wollen sie das Outfit in ihrer Garderobe speichern?',
-  ['name_outfit'] = 'Name des outfits?',
+  ['you_paid'] = 'Du bezahlst %s EUR',
+  ['save_in_dressing'] = 'Möchtest du das Outfit in der Garderobe speichern?',
+  ['name_outfit'] = 'Name des Outfits?',
   ['saved_outfit'] = 'Das outfit wurde gespeichert',
 }
